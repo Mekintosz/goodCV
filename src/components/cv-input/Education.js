623e9d224079155525e3cc83e7bfbee007efe442
Mekintosz <134439256+Mekintosz@users.noreply.github.com>
2024-01-25 15:21:37 +0100

    add exapleCv.js

---
+++ src/components/cv-input/Education.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function EducationSection () {
+    return (
+        <div>
+            <label>
+                <input>
+                </input>
+            </label>
+            <label>
+                <input>
+                </input>
+            </label>
+            
+        </div>
+    )
+}
---
+++ src/components/cv-input/exampleCv.js
@@ -0,0 +1,46 @@
+const exampleCv = {
+    0: {
+        id: 0,
+        title: "personal info",
+        firstName: "Andy",
+        lastName: "Sliwinski",
+        profesionalTitle: "Front-end developer",
+        mobile: "123 456 789",
+        email: "andy.developer@devmail.com",
+        address: "Bridge Street 7, Bristol, UK"
+    },
+    1: {
+        id: 1,
+        title: "experience",
+        childsIds: [3,4]
+    },
+    2: {
+        id:2,
+        title: "education",
+        childsIds: [5]
+    },
+    3: {
+        id: 3,
+        title: "experience section",
+        position: "UI Designer",
+        company: "Peer Inc.",
+        from: "september 2011",
+        to: "august 2013",
+        description: "Design desktop applications for restaurants and car dealers."
+    },
+    4: {
+        id: 4,
+        title: "experience section",
+        position: "UI Senior Designer",
+        company: "Peer Inc.",
+        from: "september 2011",
+        to: "august 2013",
+        description: "Design desktop and mobile applications for a major corporation."
+    },
+    5: {
+        id: 5,
+        title: "education section",
+        couresTitle: "Computer Science",
+        establishment: "London University",
+        yearCompleted: "2010"
+    }}
